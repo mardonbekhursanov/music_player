53f0f1ff2752f9272bb748f901579f3323bb4981
mardonbekhursanov <mardonbekhursanov91@gmail.com>
2025-12-10 16:56:57 +0500

    force false

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ const start = async () => {
         console.log("Database not connected ❌");
         
     }
-    await db.sequelize.sync({force: true})
+    await db.sequelize.sync({force: false})
 
     await adminRegister()
     app.listen(PORT, (err)=>{
